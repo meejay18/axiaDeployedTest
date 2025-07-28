ce310784062a9e7a44b40d68d6e6931887c751d3
digbanmije18@gmail.com <digbanmije18@gmail.com>
2025-07-28 04:30:01 +0100

    changes

--- route/user.route.js
+++ route/user.route.js
@@ -1,36 +1,36 @@
-import express from 'express';
+import express from 'express'
 import {
- createUser,
- deleteUser,
- getAllUsers,
- getOneUser,
- loginUser,
- updateUser,
- statusCode,
- singleFile,
- arrayFile,
- multipleFiles,
-} from '../controller/user.controller.js';
-const route = express.Router();
+  createUser,
+  deleteUser,
+  getAllUsers,
+  getOneUser,
+  loginUser,
+  updateUser,
+  statusCode,
+  singleFile,
+  arrayFile,
+  multipleFiles,
+} from '../controller/user.controller.js'
+const route = express.Router()
 
-import multer from 'multer';
-const upload = multer({ dest: 'uploads/' });
+import multer from 'multer'
+const upload = multer({ dest: 'uploads/' })
 
 const moreField = upload.fields([
- { name: 'previewPic', maxCount: 1 },
- { name: 'detailedPix', maxCount: 1 },
- { name: 'video', maxCount: 1 },
-]);
+  { name: 'previewPic', maxCount: 1 },
+  { name: 'detailedPix', maxCount: 1 },
+  { name: 'video', maxCount: 1 },
+])
 
-route.post('/createuser', createUser);
-route.get('/getallusers', getAllUsers);
-route.get('/getoneuser/:id', getOneUser);
-route.put('/updateuser/:id', updateUser);
-route.post('/loginuser', loginUser);
-route.delete('/deleteuser/:id', deleteUser);
-route.post('/statuscode', statusCode);
-route.post('/single', upload.single('dp'), singleFile);
-route.post('/array', upload.array('dp', 3), arrayFile);
-route.post('/multiple', moreField, multipleFiles);
+route.post('/', createUser)
+route.get('/getallusers', getAllUsers)
+route.get('/getoneuser/:id', getOneUser)
+route.put('/updateuser/:id', updateUser)
+route.post('/loginuser', loginUser)
+route.delete('/deleteuser/:id', deleteUser)
+route.post('/statuscode', statusCode)
+route.post('/single', upload.single('dp'), singleFile)
+route.post('/array', upload.array('dp', 3), arrayFile)
+route.post('/multiple', moreField, multipleFiles)
 
-export default route;
+export default route
